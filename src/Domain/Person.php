3bcdfe81ad99896c4b8ef5ab2395926735d509f5
Loureiro00 <pedrohstloureiro@gmial.com>
2024-10-05 13:56:10 -0300

    Added Person entity as a base class for managing names in the system

---
+++ src/Domain/Person.php
@@ -0,0 +1,19 @@
+<?php
+namespace Application\Domain;
+
+class Person {
+    protected $name;
+
+    public function __construct(string $name) {
+        $this->name = $name;
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+
+    public function setName(string $name): void {
+        $this->name = $name;
+    }
+}
+
